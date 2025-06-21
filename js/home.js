ce852be53eaf04383251fcf7797ecb1cf38a0ca0
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2025-06-21 13:03:41 +0700

    Customer (#5)

---
+++ js/home.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var cards = document.querySelectorAll('.card');
+  cards.forEach(function(card) {
+    card.style.cursor = 'pointer';
+    card.addEventListener('click', function() {
+      window.location.href = 'chi_tiet_san.html';
+    });
+  })});
